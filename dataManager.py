fixed weights, fixed lheht plots, fixed ggh weights, adding sensitivity plots but no sensitivity calculation yet

dataManager.py
@@ -57,12 +57,12 @@ BGenDict = dict(zip(BGenPaths, BGenWeight))
 bEnrDict = dict(zip(bEnrPaths, bEnrWeight))
 
 JetVars = [
-    'FatJet_pt', 
-    'FatJet_eta', 
-    'FatJet_mass', 
-    'FatJet_btagCSVV2', 
-    'FatJet_btagDeepB', 
-    'FatJet_msoftdrop', 
+    'FatJet_pt',
+    'FatJet_eta',
+    'FatJet_mass',
+    'FatJet_btagCSVV2',
+    'FatJet_btagDeepB',
+    'FatJet_msoftdrop',
     'FatJet_btagDDBvL',
     'FatJet_deepTagMD_H4qvsQCD',
     ## high discriminatroy
@@ -110,11 +110,11 @@ allVars.sort()
 disc = 'm'
 standardVars = [
     'FatJet_pt',
-    'FatJet_eta', 
-    'FatJet_mass', 
-    'FatJet_btagCSVV2', 
-    'FatJet_btagDeepB', 
-    'FatJet_msoftdrop', 
+    'FatJet_eta',
+    'FatJet_mass',
+    'FatJet_btagCSVV2',
+    'FatJet_btagDeepB',
+    'FatJet_msoftdrop',
     'FatJet_btagDDBvL',
     'FatJet_deepTagMD_H4qvsQCD'
     ]
@@ -136,7 +136,7 @@ mediumDiscVars = [
     ]
 
 
-if disc == None:
+if disc == 'h':
     trainVars = standardVars
 elif disc == 'h':
     trainVars = standardVars + highDiscVars
@@ -184,7 +184,7 @@ def getnSVCounts(data, events):
     ## sveta and svphi are arrays of values corresponding to the event
     sveta = events.array('SV_eta')[eventidx]
     svphi = events.array('SV_phi')[eventidx]
-    
+
     dr = np.sqrt(np.power(jeteta - sveta, 2) + np.power(jetphi - svphi, 2))
     dr = pd.DataFrame(dr)
     nSVcounts = (dr < 0.8).sum(axis=1)
@@ -224,7 +224,7 @@ def processData(filePath, tag):
         try:
             data[var] = pd.DataFrame(events.array(var))
             ## makes eta positive only
-            if 'eta' in var: 
+            if 'eta' in var:
                 data[var] = data[var].abs()
         except:
             continue'''
@@ -312,12 +312,12 @@ def processData(filePath, tag):
     if disc != None:
         # eventidx = data.FatJet_pt.index
         # maxptjetidx = colidx
-    
+
         # jeteta = events.array('FatJet_eta')[eventidx, maxptjetidx]
         # jetphi = events.array('FatJet_phi')[eventidx, maxptjetidx]
         # sveta = events.array('SV_eta')[eventidx]
         # svphi = events.array('SV_phi')[eventidx]
-    
+
         # dr = np.sqrt(np.power(jeteta - sveta, 2) + np.power(jetphi - svphi, 2))
         # dr = pd.DataFrame(dr)
         # nSVcounts = (dr < 0.8).sum(axis=1)
@@ -337,11 +337,11 @@ def processData(filePath, tag):
 
 
 
-    ## LHE weights
+    ## assign (where applicable) LHE_weights, QCD_correction, Xsec weights
     #if tag == 'data':
     #    maxPtData['LHE_weights'] = 1
     if tag == 'ggH':
-        maxPtData['LHE_weights'] = 1#43920/999000
+        maxPtData['LHE_weights'] = 1
         wgt = 3.9 - 0.4*np.log(maxPtData.FatJet_pt)/np.log(2)
         wgt[wgt<0.1] = 0.1
         maxPtData['ggH_weights'] = wgt
@@ -424,53 +424,6 @@ def processData(filePath, tag):
 
         maxPtData = maxPtData.assign(final_weights = maxPtData['LHE_weights'])
 
-    ## npvs Ratio weights
-    ## we only need pileup and lumi for dataVsMC
-    '''if tag != 'data':
-        for i in range(len(ptkeys)-1):
-            for j in range(len(ipkeys)-1):
-                for k in range(len(npvsGkeys)):
-                    #print('{} {} {}'.format(ptkeys[i],ipkeys[j],npvsGkeys[k]))
-                    maxPtData.loc[(maxPtData.Muon_pt >= ptkeys[i]) &
-                                  (maxPtData.Muon_pt < ptkeys[i+1]) &
-                                  (maxPtData.Muon_IP >= ipkeys[j]) &
-                                  (maxPtData.Muon_IP < ipkeys[j+1]) &
-                                  (maxPtData.Muon_eta < 1.5) &
-                                  (maxPtData.PV_npvsGood == k+1),
-                                  'PU_weights'] = muonR[ptkeys[i]][ipkeys[j]]['L'][k]
-                    maxPtData.loc[(maxPtData.Muon_pt >= ptkeys[i]) &
-                                  (maxPtData.Muon_pt < ptkeys[i+1]) &
-                                  (maxPtData.Muon_IP >= ipkeys[j]) &
-                                  (maxPtData.Muon_IP < ipkeys[j +1]) &
-                                  (maxPtData.Muon_eta >= 1.5) &
-                                  (maxPtData.PV_npvsGood == k+1),
-                                  'PU_weights'] = muonR[ptkeys[i]][ipkeys[j]]['H'][k]
-
-
-        ## lumi weights
-        for i in range(len(ptkeys)-1):
-            for j in range(len(ipkeys)-1):
-                maxPtData.loc[(maxPtData.Muon_pt >= ptkeys[i]) &
-                             (maxPtData.Muon_pt < ptkeys[i+1]) &
-                             (maxPtData.Muon_IP >= ipkeys[j]) &
-                             (maxPtData.Muon_IP < ipkeys[j+1]) &
-                             (maxPtData.Muon_eta < 1.5),
-                             'lumi_weights'] = muonL[ptkeys[i]][ipkeys[j]]['L']
-                maxPtData.loc[(maxPtData.Muon_pt >= ptkeys[i]) &
-                             (maxPtData.Muon_pt < ptkeys[i+1]) &
-                             (maxPtData.Muon_IP >= ipkeys[j]) &
-                             (maxPtData.Muon_IP < ipkeys[j +1]) &
-                             (maxPtData.Muon_eta >= 1.5),
-                             'lumi_weights'] = muonL[ptkeys[i]][ipkeys[j]]['H']
-
-        ## ultimate weight
-        maxPtData = maxPtData.assign(final_weights =
-                                     maxPtData['lumi_weights']*
-                                     maxPtData['PU_weights']*
-                                     maxPtData['LHE_weights'])'''
-
-
-    #maxPtData = maxPtData.assign(final_weights=maxPtData['LHE_weights'])
 
     # tag the thing
     maxPtData['tag'] = tag
@@ -482,25 +435,6 @@ def processData(filePath, tag):
 
     return maxPtData
 
-
-## cuts
-## On top of the regular selection, you should require the existence of at
-## least one muon passing softId, with |eta| < 2.4, ip3d < 0.5, pT > 6, and
-## (dxy/dxyErr) > 2, in both data and MC.
-
-            # muons.cut(muons.softId > 0.9)
-            # muons.cut(abs(muons.eta) < 2.4)
-            # muons.cut(muons.pt > 7)
-            # muons.cut(muons.ip > 2)
-            #     jets.cut(jets.pt > 170)#240)#170)
-            #     jets.cut(abs(jets.eta)<2.4)
-            #     jets.cut(jets.DDBvL > 0.8)#0.8)#0.6)
-            #     jets.cut(jets.DeepB > 0.4184)
-            #     jets.cut(jets.msoft > 90)#90)#0.25)
-            #     #
-            #     jets.cut(jets.mass > 90)
-            #     jets.cut(jets.msoft < 200)
-            #     jets.cut(jets.npvsG >= 1)
 
 
 

dataVsMC.py
@@ -5,6 +5,7 @@ from analib import PhysObj, Event
 from info import BGenFileNames, bEnrFileNames
 
 import dataVsMC_DataManager as DM #import processData, jetVars, muonVars, PVVars, allVars, dataPath, ggHPath, bEnrPaths, BGenPaths, TTJetsPaths,
+from dataManager import trainVars # this is so we can run our thing through the BDT XML correctly
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import mplhep as hep
@@ -16,12 +17,15 @@ import uproot
 plt.style.use(hep.style.CMS)
 
 
+## if reading from rootfiles, set true. if already have pickle of dataframe, set false
+root = False
 
-## monte carlo backgrounds
-#BGenDf = processData(BGenPath, 'BGen')
-## uncomment below if using multiple bg MC files
-
-root = True
+## function to add a BDTScore column to each of the background/signal/data DF
+loadedModel = pickle.load(open('Htoaa_BDThigh+medium disc.pkl', 'rb'))
+def analyze(dataDf):
+    prediction = loadedModel.predict_proba(dataDf[trainVars])
+    dataDf = dataDf.assign(BDTScore=prediction[:,1])
+    return dataDf
 
 if root:
     ## monte carlo ggH signal
@@ -136,6 +140,17 @@ else:
     else:
         dataDf = pickle.load(open('dataVsMC/dataDf.pkl', 'rb'))
 
+ggHDf = analyze(ggHDf)
+BGenDf = analyze(BGenDf)
+bEnrDf = analyze(bEnrDf)
+TTJetsDf = analyze(TTJetsDf)
+ZJetsDf = analyze(ZJetsDf)
+WJetsDf = analyze(WJetsDf)
+if DM.JetHT:
+    JetHTDf = analyze(JetHTDf)
+else:
+    dataDf = analyze(dataDf)
+
 
 
 dfdict = { 'WJets': WJetsDf,
@@ -163,24 +178,26 @@ for var in cols:
     xmin = list()
     xmax = list()
 
-    ## old way. when only doing 1 background
-    #minBGen, maxBGen = np.percentile(BGenDf[var], [0, 99.8])
-    #minData, maxData = np.percentile(dataDf[var], [0, 99.8])
-
     for dfkey, df in dfdict.items():
         xmintmp, xmaxtmp = np.percentile(df[var], [0,99.8])
         xmin.append(xmintmp)
         xmax.append(xmaxtmp)
 
+
     if 'H4qvsQCD' in var:
         #range_local = (0, max(maxBGen, maxData))
         range_local = (0, max(xmax))
+    elif 'FatJet_pt'==var:
+        range_local = (240, 700)
+    elif 'BDTScore'==var:
+        range_local = (0,0.7)
     else:
         #range_local = (min(minBGen, minData), max(maxBGen, maxData))
         range_local = (min(xmin), max(xmax))
 
 
-    hist_params = {'density': True, 'histtype': 'bar', 'range' : range_local, 'bins':nbins, 'stacked':True}
+    density = True
+    hist_params = {'density': density, 'histtype': 'bar', 'range' : range_local, 'bins':nbins, 'stacked':True}
 
 
     toplot = pd.DataFrame()
@@ -193,15 +210,23 @@ for var in cols:
         toplotweights[dfkey] = df['final_weights']
         toplotlabel.append(dfkey)
 
-    ax.hist(toplot.values, weights=toplotweights.values,label=toplotlabel, **hist_params)
+    if DM.JetHT:
+        ax.hist(JetHTDf[var].values, label='JetHT', histtype='step', density=density, bins=nbins, linewidth=3, color='k',
+                range=range_local)
+    else:
+        ax.hist(dataDf[var].values, label='parkedData', histtype='step', density=density, bins=nbins, linewidth=3, color='k',
+                range=range_local)
+
+    ax.hist(ggHDf[var].values, label='GGH', histtype='step', density=True, bins=nbins, linewidth=3, color='r', range=range_local)
+
+    ## making color palette for the QCD stakcs
+    pal = ['#603514', '#b940f2','#ec6f38', '#6acaf8', '#82f759']
+    ax.hist(toplot.values, weights=toplotweights.values,label=toplotlabel, color=pal, **hist_params)
     #ax.hist(BGenDf[var].values, weights=BGenDf['final_weights'].values, label='BGen', **hist_params)
     #ax.hist(bEnrDf[var].values, weights=bEnrDf['final_weights'].values, label='bEnr', **hist_params)
     #ax.hist(TTJetsDf[var].values, weights=TTJetsDf['final_weights'].values, label='TTJets', **hist_params)
-    ax.hist(ggHDf[var].values, label='GGH', histtype='step', density=True, bins=nbins, linewidth=3, color='r')
-    if DM.JetHT:
-        ax.hist(JetHTDf[var].values, label='JetHT', histtype='step', density=True, bins=nbins, linewidth=3, color='k')
-    else:
-        ax.hist(dataDf[var].values, label='parkedData', histtype='step', density=True, bins=nbins, linewidth=3, color='k')
+
+
 
     #ax.hist(dataDf[var], label='Data',
     ax.set_title(var )# + ' JetHT')
@@ -212,9 +237,54 @@ for var in cols:
     else:
         filedest = 'dataVsMCDist/Parked/{}.png'
     plt.savefig(filedest.format(var))
-    #plt.clf()
     plt.show()
     plt.close()
+
+
+########################### Sensitivity Plots ##########################
+sortedggH = ggHDf.sort_values(by='BDTScore', axis=0, kind='mergesort')
+histHeight = sortedggH.final_weights.sum()/10
+edges = [0]
+cumuSum = 0
+edgesloc = []
+for i in range(sortedggH.BDTScore.size):
+    cumuSum = cumuSum + sortedggH.final_weights.iloc[i]
+    if cumuSum > histHeight:
+        edges.append(sortedggH.BDTScore.iloc[i-1])
+        cumuSum = sortedggH.final_weights.iloc[i]
+        edgesloc.append(i-1)
+edges.pop()
+edges.append(sortedggH.BDTScore.iloc[-1])
+
+fig, ax = plt.subplots(figsize=(8,8))
+sighist = ax.hist(sortedggH.BDTScore, bins=edges,
+                  weights=sortedggH.final_weights, histtype='step',
+                  label='ggH', log=True, color='r', linewidth=3)
+datahist = ax.hist(dataDf.BDTScore, bins=edges, histtype='step',
+                   label='data', log=True, color='k', linewidth=3)
+
+toplot = pd.DataFrame()
+toplotweights = pd.DataFrame()
+toplotlabel = list()
+for dfkey, df in dfdict.items():
+    toplot[dfkey] = df['BDTScore']
+    toplotweights[dfkey] = df['final_weights']
+    toplotlabel.append(dfkey)
+
+
+bghist = ax.hist(toplot.values, weights=toplotweights.values, bins=edges,
+        label=toplotlabel, color=pal, log=True, histtype='bar', stacked=True)
+
+
+# !!! TODO
+## figure out how to compute the sensitivity for the stacked plot bitch
+
+ax.legend(loc='best')
+ax.set_title('sensitivty')
+plt.show()
+
+
+########################################################################
 
 
     ## when used to do bEnr and BGen separately
@@ -234,12 +304,10 @@ for var in cols:
     # plt.savefig('dataVsMCDist_fixed/bEnr/{}_bEnrVsData.png'.format(var))
     # plt.close()
 
-'''nbins=40
-
+nbins=40
 
 #del dfdict['JetHTDf']
-del dfdict['TTJets']
-
+hist_params = {'density': density, 'histtype': 'bar', 'range' : range_local, 'bins':nbins, 'stacked':True}
 for dfkey, dfvalue in dfdict.items():
     fig, ax = plt.subplots(figsize=(10,8))
     range_local = np.percentile(dfvalue['LHE_HT'], [0,99.8])
@@ -247,10 +315,10 @@ for dfkey, dfvalue in dfdict.items():
             **hist_params)
     ax.set_title('LHE_HT ' + dfkey)
     ax.grid()
-    plt.savefig(f'dataVsMCDist/JetHT/LHE_HT_{dfkey}.png')
+    plt.savefig(f'dataVsMCDist/Parked/LHE_HT_{dfkey}.png')
     plt.show()
     plt.close()
-    #plt.clf()'''
+    #plt.clf()
 
 
 ## old way when bgen benr plotted separate

dataVsMC_DataManager.py
@@ -295,6 +295,10 @@ def processData (filePath, tag): #JetHT=False):
             ## LHE_weights
             if tag == 'ggH':
                  maxPtData['final_weights'] = 1
+                 wgt = 3.9 - 0.4*np.log(maxPtData.FatJet_pt)/np.log(2)
+                 wgt[wgt<0.1] = 0.1
+                 maxPtData['ggH_weights'] = wgt
+                 maxPtData['final_weights'] = maxPtData['LHE_weights'] * maxPtData['ggH_weights']
             elif tag == 'BGen':
                 maxPtData['LHE_weights'] = BGenDict[filePath]
                 '''maxPtData.loc[(maxPtData['LHE_HT']>200) & (maxPtData['LHE_HT']<=300),
@@ -387,7 +391,7 @@ def processData (filePath, tag): #JetHT=False):
             elif tag == 'TTJets':
                 maxPtData = maxPtData.assign(final_weights=831760.0/10244307)
 
-            if not JetHT:
+            if not JetHT and tag != 'ggH':
                 ## npvs Ratio weights
                 for i in range(len(ptkeys)-1):
                     for j in range(len(ipkeys)-1):