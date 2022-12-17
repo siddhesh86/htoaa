ec206cafeef45ca8a74b9e0862946dac0f511a98
Siddhesh Sawant <sawantsiddhesh08@gmail.com>
2022-12-17 17:48:00 +0530

    Script to generate all steps of SUSY_HToAATo4B samples locally

---
+++ MCGenerationScript_H_M125_a01_M25.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+printf 'Run ' NEvents_toUse: 4000 
+time source generate_RunIISummer20UL18wmLHEGEN.sh  input.root  ${Dir_store}/${sampleName_toUse}/${ERA}/${DatasetType}/sample_0.root  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ee
