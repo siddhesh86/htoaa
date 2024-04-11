a7ed17203d48874728a9612e6b3d9eda741e6d10
Siddhesh Sawant <siddhesh.gopichand.sawant.cern.ch>
2024-04-11 09:16:19 -0500

    Working version for small width intermediate mA points

--- generate_params_MCGeneration_HToAATo4B_M-x.sh
+++ generate_params_MCGeneration_HToAATo4B_M-x.sh
@@ -14,17 +14,30 @@ HiggsPtMin=150 # 150 250 350
 #masspoints=(12 15 20 25 30 35 40 45 50 55 60)
 #masspoints=(8.5 9.0 9.5 10.0 10.5 11.0 11.5 12.5 13.0 13.5 14.0 16.0 17.0 18.5 21.5 23.0 27.5 32.5 37.5 42.5 47.5 52.5 57.5 62.5)
 #masspoints=(47.5)
-masspoints=(52.5)
+#masspoints=(32.5)
+#masspoints=(52.5)
+#masspoints=(8.5 9.0 9.5 10.0 10.5 11.0 11.5 12.5 13.0 13.5 14.0 16.0 17.0 18.5 21.5 23.0 27.5 37.5 42.5 57.5 62.5) #(32.5) #(52.5)
+# compensating failed jobs
+#masspoints=(47.5 52.5 57.5 62.5)
 
 ERA="RunIISummer20UL18"
 NEvents=500
 #NEvents=10
 
 # set first (SampleNumber_First) to last (SampleNumber_Last) MC sample file numbers to be produced in this round of submission/execution.
-SampleNumber_First=0
-SampleNumber_Last=199
-#SampleNumber_First=17
-#SampleNumber_Last=19
+#SampleNumber_First=1400 #700
+#SampleNumber_Last=1899
+
+# compensating failed jobs
+#masspoints=(47.5 52.5 57.5 62.5)
+#SampleNumber_First=1900
+#SampleNumber_Last=2099
+#masspoints=(37.5 42.5 47.5 52.5 57.5 62.5)
+#SampleNumber_First=2100
+#SampleNumber_Last=2299
+masspoints=(18.5 21.5 23.0 27.5 32.5 37.5 42.5 47.5 52.5 57.5 62.5)
+SampleNumber_First=2300
+SampleNumber_Last=2499
 
 XRootDRedirector="xrootd-cms.infn.it"
 
